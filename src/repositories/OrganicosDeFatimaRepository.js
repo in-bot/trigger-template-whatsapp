217635b54f77178286c3ffb0ad11370284465940
Tom-Inbot <tomas.martinez@inbot.com.br>
2024-06-27 10:03:16 -0300

    Update OrganicosDeFatimaRepository.js
    Atualizei para ser 50 requests cada minuto para não ter erro de timeout.

--- src/repositories/OrganicosDeFatimaRepository.js
+++ src/repositories/OrganicosDeFatimaRepository.js
@@ -18,29 +18,45 @@ const fetchPage = async (input) => {
     },
   };
   requestOptions.data.pesquisa = input?.nome;
-  try {
+   try {
     const resp = await axios(requestOptions);
-    if(resp.data.retorno.status==="Erro"){
-        return;
+    if (resp.data.retorno.status === "Erro") {
+      return console.log(resp.data.retorno.erros);
     }
     const body = resp?.data?.retorno?.contatos[0]?.contato;
+    if (resp?.data?.retorno?.contatos[0].length > 1) {
+      return console.log("Cadastro Duplicado");
+    }
     input.id_contato = body.id;
-    input.fone = body.fone.replace("(","55").replace(")","").replace(" ","").replace("-","");
+    input.fone = body.fone
+      .replace("(", "55")
+      .replace(")", "")
+      .replace(" ", "")
+      .replace("-", "");
     input.cpf_cnpj = body.cpf_cnpj;
+    console.log(new Date(), requestOptions.data.pesquisa, `Customer: ${input}`);
     return input;
   } catch (err) {
-    console.log(err)  }
+    console.log(err);
+  }
 };
 
 const fetchAllPages = async () => {
   let arr = [];
+  console.log("Waiting 1 minute...");
+  await delay(60000);
   const mergedResponses = await fetchAllPagesCustomer();
-  console.log(mergedResponses)
+  console.log(mergedResponses);
+  console.log("Waiting 1 minute...");
+  await delay(60000);
   for (let i = 0; i < mergedResponses.length; i++) {
-    const arrValues = await fetchPage(mergedResponses[i])
-    if(arrValues!==undefined && arrValues?.fone!==""){
-      arr.push(arrValues)
-      console.log(new Date(), `Customer: ${JSON.stringify(arrValues)}`)
+    if ((i + 1) % 50 === 0) {
+      await delay(60000);
+    }
+
+    const arrValues = await fetchPage(mergedResponses[i]);
+    if (arrValues !== undefined && arrValues?.fone !== "") {
+      arr.push(arrValues);
     }
   }
   return arr;
