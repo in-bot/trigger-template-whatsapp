717ac535fb1489a5217d11fbd3827c0551958c62
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2024-08-16 09:14:06 -0300

    Adicionado rotina para criar disparos automaticos para aramis no sabado as 10:10h

--- src/repositories/TemplateTriggerRepository.js
+++ src/repositories/TemplateTriggerRepository.js
@@ -2,12 +2,13 @@ const db = require("../config/dbWhatsApp");
 
 module.exports = {
     getByStatus: (channel) => {
+        console.log(channel)
         return new Promise((accept, reject) => {
             db.query(
-                "SELECT * FROM templateTriggering WHERE status = 'aguardando' AND channel=?;",[channel],
+                "SELECT * FROM templateTriggering WHERE status = 'aguardando';",
                 (error, results) => {
                     if (error) {
-                        return reject("Request getAll error");
+                        return reject("Request getByStatus error");
                     }
                     accept(results);
                 }
@@ -20,7 +21,7 @@ module.exports = {
                 "SELECT * FROM templateTriggeringCustomer where campaign_id = ?;",[id],
                 (error, results) => {
                     if (error) {
-                        return reject("Request getAll error");
+                        return reject("Request getCustomerByIdTemplate error");
                     }
                     accept(results);
                 }
